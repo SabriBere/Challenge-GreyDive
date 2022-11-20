49d2e8393223d72ca246238eb10a039022a65cba
Sabrina Demetrio <95036708+SabriBere@users.noreply.github.com>
2022-11-20 11:45:44 -0300

    inicializando proyecto en back

---
+++ back/controllers/recordersControllers.js
@@ -0,0 +1,9 @@
+const recordersService = require("../service/recordersService")
+
+class recordersControllers {
+    static async allRecords(req, res) {
+      console.log("Acá entro a controllers, falta lógica");
+    }
+  }
+  
+  module.exports = recordersControllers;
