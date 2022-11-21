43393746a13730dabe8b81f051c9f2d7213f47d2
SabriBere <sabrinab786@gmail.com>
2022-11-20 22:11:31 -0300

    generadon rutas del back, cofig redux y conectando back c/ front

--- front/src/index.js
+++ front/src/index.js
@@ -4,13 +4,17 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import theme from "./theme/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <Provider store={store}>
   <ChakraProvider theme={theme} >
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </ChakraProvider>
+  </Provider>
 );
