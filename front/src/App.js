6ec085a25a8e12e37442914c2ad937717f6d3146
Sabrina Demetrio <95036708+SabriBere@users.noreply.github.com>
2022-11-22 22:46:30 -0300

    paginación de las cards y captura de los inputs opinión

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Flex } from "@chakra-ui/react";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
 import Questions from "./pages/Questions";
@@ -9,21 +9,19 @@ import Pagination from "./components/Questions/Pagination";
 const App = () => {
   return (
     <Flex
-    w="100%"
-    // boxSizing="border-box"
-    flexDirection="column"
-    bg="fondo"
-    justifyContent="center"
-    alignContent="center"
+      w="100%"
+      flexDirection="column"
+      bg="fondo"
+      justifyContent="center"
+      alignContent="center"
     >
       <Navbar />
       <Flex justifyContent="center" alignItems="center">
-      <Routes>
-        <Route path="/" element={<Home />} /> 
-        <Route path="/questions" element={<Questions />} />
-        <Route path="/hola" element={<Pagination />} />
-        
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/questions" element={<Questions />} />
+          <Route path="/hola" element={<Pagination />} />
+        </Routes>
       </Flex>
     </Flex>
   );
