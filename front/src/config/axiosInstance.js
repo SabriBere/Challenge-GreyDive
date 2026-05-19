a3fcda8902089e5162fd29207414afb6fc738081
Sabrina Demetrio <95036708+SabriBere@users.noreply.github.com>
2026-05-19 17:44:06 -0300

    fix: migrate frontend from CRA to Vite

--- front/src/config/axiosInstance.js
+++ front/src/config/axiosInstance.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NODE_ENV === "production" ? "/" : "http://localhost:3000/";
-
 const config = {
-  baseURL,
+  baseURL: "/",
   withCredentials: false,
 };
 
